bf53126466111223c55e3ee3aad37b9e15ec33ef
Serg-Ale <sergioalexandre0716@gmail.com>
2024-05-29 14:15:36 -0300

    feat: add initial nav functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,35 @@
+import { Link } from "react-router-dom";
+import "./styles/Nav.css";
+
 const Nav = () => {
   return (
-    <nav>
-      <h1>Isso é um nav</h1>
-    </nav>
+    <asside>
+      <nav>
+        <h3>Fundamentos de Algoritmos</h3>
+        <ul>
+          <li>
+            <Link to="/fundamentos/conceitos-basicos">
+              Aula 1: Introdução à Linguagem C e Conceitos Básicos de Algoritmos
+            </Link>
+          </li>
+          <li>
+            <Link to="/fundamentos/variaveis">
+              Aula 2 - Constantes, Variáveis e Tipos de Dados
+            </Link>
+          </li>
+          <li>
+            <Link to="/fundamentos/expressoes">
+              Aula 3 - Expressões Aritméticas, Relacionais e Lógicas
+            </Link>
+          </li>
+          <li>
+            <Link to="/fundamentos/entrada-saida">
+              Aula 4 - Comandos de Entrada e Saída
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </asside>
   );
 };
 
